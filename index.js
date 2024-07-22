0ffbf34adf845d4925eb27d58c0e84583851b648
Hakim 'Azizan <hazizan22@gmail.com>
2024-07-22 02:13:49 +0000

    feat: add solution

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const bodyParser = require('body-parser');
+const dns = require('dns');
+const {URL} = require('url');
+const fs = require('fs');
+
 const app = express();
+const url_list_path = "./url_list.json";
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
@@ -9,6 +15,8 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.get('/', function(req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
@@ -22,3 +30,60 @@ app.get('/api/hello', function(req, res) {
 app.listen(port, function() {
   console.log(`Listening on port ${port}`);
 });
+
+app.post('/api/shorturl', (req,res)=>{
+  
+  var req_url = req.body.url;
+  try {
+    var parsed_url = new URL(req_url);
+    var hostname = parsed_url.hostname;
+
+    dns.lookup(hostname, (err, addr, fam)=>{
+
+      if (err){
+        res.json({error: "Invalid url"});
+      }
+      else checkURL(req_url, res);
+    });
+  }
+  catch (error){
+    res.json({error: "Invalid url"});
+  }
+  
+})
+
+app.get('/api/shorturl/:shorturl', (req,res)=>{
+
+  var req_short_url = req.params.shorturl;
+  fs.readFile(url_list_path, 'utf-8', (err, data)=>{
+    if (err) return res.status(500).send('Error reading URL list');
+    const url_list = JSON.parse(data);
+    const target_url = url_list.urls.find(entry=>entry.short===req_short_url);
+    if (target_url) return res.redirect(target_url.original);
+    else return res.json({"error":"No short URL found for the given input"});
+  })
+})
+
+var checkURL = (url, res) => {
+  fs.readFile(url_list_path, 'utf-8', (err, data)=>{
+    if (err) return res.status(500).send('Error reading URL list');
+    
+    const url_list = JSON.parse(data);
+    const target_url = url_list.urls.find(entry => entry.original === url);
+
+    if (target_url){
+      res.json({"original_url": url, "short_url": target_url.short});
+    }
+    else {
+      const newShort = (url_list.urls.length + 1).toString();
+      const newEntry = {original: url, short: newShort};
+
+      url_list.urls.push(newEntry);
+
+      fs.writeFile(url_list_path, JSON.stringify(url_list,null,2), err => {
+        if (err) res.status(500).send('Error saving URL list');
+        else res.json({"original_url": url, "short_url": newShort});
+      });
+    }
+  })
+}
